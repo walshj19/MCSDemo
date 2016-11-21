10da556f165f3e41d62f634511f9d105670a34b9
walshj19 <walshj19@tcd.ie>
2016-11-21 14:56:36 +0000

    Started Adding testing and DAO

---
+++ app/src/main/java/d16124837/mydit/ie/mcsdemo/DAO.java
@@ -0,0 +1,10 @@
+package d16124837.mydit.ie.mcsdemo;
+
+/**
+ * Created by james on 21/11/2016.
+ * This class is a wrapper for the android sql lite database on the device.
+ */
+
+public class DAO {
+
+}
---
+++ app/src/test/java/d16124837/mydit/ie/mcsdemo/DAOTest.java
@@ -0,0 +1,11 @@
+package d16124837.mydit.ie.mcsdemo;
+
+/**
+ * Created by james on 21/11/2016.
+ */
+
+public class DAOTest {
+	DAO dao = new DAO();
+
+
+}
