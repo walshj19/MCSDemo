f8866f2a411bea71bf6bfa50900d0d3e687e6ed0
James Walsh <walshj19@tcd.ie>
2016-11-21 19:43:43 +0000

    Added file access object and external storage permission

---
+++ app/src/main/java/d16124837/mydit/ie/mcsdemo/FAO.java
@@ -0,0 +1,13 @@
+package d16124837.mydit.ie.mcsdemo;
+
+/**
+ * Created by James on 21-Nov-16.
+ * This class is a wrapper for the device local file storage.
+ */
+
+// TODO: check for external storage and only use internal if it's not available
+public class FAO {
+    public FAO() {
+
+    }
+}
