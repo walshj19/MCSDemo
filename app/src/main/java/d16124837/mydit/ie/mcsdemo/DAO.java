ef5ee5eb6476a8ca55e22ec5c4ec504336790d04
James Walsh <walshj19@tcd.ie>
2016-11-23 15:46:10 +0000

    Moved DBContract to subclass of DAO and added insert method.

--- app/src/main/java/d16124837/mydit/ie/mcsdemo/DAO.java
+++ app/src/main/java/d16124837/mydit/ie/mcsdemo/DAO.java
@@ -1,8 +1,10 @@
 package d16124837.mydit.ie.mcsdemo;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
 /**
  * Created by james on 21/11/2016.
@@ -11,12 +13,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 // TODO: The database open and close operations are blocking
 // TODO: Finish defining database table
-public class DAO {
-    private class DBHelper extends SQLiteOpenHelper{
-        public static final int DATABASE_VERSION = 1;
-        public static final String DATABASE_NAME = "mcsdemo";
+class DAO {
+    /**
+     * Inserts one image into the database
+     * @param image this is the data for the image to be added to the database.
+     */
+    static void insert(Context context, ImageData image){
+        DBHelper dbHelper = new DBHelper(context);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
 
-        public DBHelper(Context context){
+        ContentValues values = new ContentValues();
+        values.put(DBContract.Image.COLUMN_NAME_PATH, image.getPath());
+
+        db.insert(DBContract.Image.TABLE_NAME, null, values);
+    }
+
+    /**
+     * This is the database helper class for the sqllite database.
+     */
+    private static class DBHelper extends SQLiteOpenHelper{
+        static final int DATABASE_VERSION = 1;
+        static final String DATABASE_NAME = "mcsdemo";
+
+        DBHelper(Context context){
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
 
@@ -36,6 +55,18 @@ public class DAO {
             String drop_tables = "DROP TABLE IF EXISTS "+DBContract.Image.TABLE_NAME;
             db.execSQL(drop_tables);
             onCreate(db);
+        }
+    }
+
+    /**
+     * This class represents the database schema.
+     */
+    private static final class DBContract {
+        // this inner class represents the schema information for a column in the database
+        static class Image implements BaseColumns {
+            static final String TABLE_NAME = "IMAGE";
+            static final String COLUMN_NAME_PATH = "IMAGE";
+            static final String COLUMN_NAME_ = "IMAGE";
         }
     }
 }
---
+++ app/src/main/java/d16124837/mydit/ie/mcsdemo/ImageData.java
@@ -0,0 +1,19 @@
+package d16124837.mydit.ie.mcsdemo;
+
+/**
+ * Created by James on 23-Nov-16.
+ *
+ * This class represents all of the information for one image and it's analysis.
+ */
+
+public class ImageData {
+    private String path;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+}
